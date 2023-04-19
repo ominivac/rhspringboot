5fd585b3cc45909f5800e4b5291614b0d534135c
ominivac <robertokbs@gmail.com>
2023-04-19 09:35:11 -0300

    Initial commit

---
+++ src/main/java/com/rh/RhApplication.java
@@ -0,0 +1,18 @@
+package com.rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+public class RhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhApplication.class, args);
+	}
+
+}
